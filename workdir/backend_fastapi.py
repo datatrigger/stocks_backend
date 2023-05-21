df98184d5007511b8614bdeb34583c6cd9e1a95c
Vincent Le Goualher <v.legoualher@gmail.com>
2023-05-21 12:36:01 +0200

    Attempt to solve connection issue

--- workdir/backend_fastapi.py
+++ workdir/backend_fastapi.py
@@ -80,8 +80,16 @@ def compute_metrics() -> pd.DataFrame:
     })
     return df_metrics
 
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 def home():
