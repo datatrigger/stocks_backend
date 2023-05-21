bcadb22d7536cb4599849f3f113983b77e4ff733
Vincent Le Goualher <v.legoualher@gmail.com>
2023-05-21 22:05:54 +0200

    Implement asynchronous calls to API

--- workdir/backend_fastapi.py
+++ workdir/backend_fastapi.py
@@ -1,94 +1,64 @@
 from fastapi import FastAPI
-import requests
 import logging
 import os
 import pandas as pd
 from datetime import date, timedelta
 from math import sqrt
+import asyncio
+from itertools import repeat, product
+import httpx
 
 api_key = os.environ.get("API_KEY") # Fetched from GCP Secrets, by GCP Cloud Run
 tickers = {"Google": "GOOG", "Amazon": "AMZN", "Microsoft": "MSFT"}
-bdays_window = 10
+window = 10
 # Charts params
 colors = {"Google": "#8e5ea2", "Amazon": "#3cba9f", "Microsoft": "#e8c3b9"}
 fill = False
 
-def previous_bdays(window: int = bdays_window - 1) -> list[str]:
+def previous_bdays() -> list[str]:
     """Return the last `window` business days, up to yesterday (at most)"""
+    bdays_window = window - 1
+    today = date.today().strftime("%Y-%m-%d")
     yesterday = date.today() - timedelta(days = 1)
-    previous_bdays = pd.bdate_range(end = yesterday, periods = window).strftime("%Y-%m-%d")
-    return previous_bdays.tolist()
+    previous_bdays = pd.bdate_range(end = yesterday, periods = bdays_window).strftime("%Y-%m-%d")
+    return previous_bdays.tolist() + [today]
 
-def current_price(company: str) -> float | None:
-    """Fetch the last know trading price for the given `company`"""
-    try:
-        ticker = tickers[company]
-    except KeyError:
-        logging.error("Unregistered company. Unable to fetch current price.")
-        return
-    query = f"https://api.polygon.io/v2/last/trade/{ticker}?apiKey={api_key}"
+async def fetch_price(company_day: tuple[str, str], client: httpx.AsyncClient) -> None:
+    """Fetch the closing price on a previous business day, or today's current price"""
+    company, day = company_day
+    today = date.today().strftime("%Y-%m-%d")
+    print(f"Making request for {company} price on {day}")
+    if day == today:
+        response = await client.get(f"https://api.polygon.io/v2/last/trade/{tickers[company]}?apiKey={api_key}")
+        price = response.json()["results"]["p"]
+    else:
+        response = await client.get(f"https://api.polygon.io/v1/open-close/{tickers[company]}/{day}?adjusted=true&apiKey={api_key}")
+        price = float(response.json()["close"])
+    return company, day, price
 
-    try:
-        response = requests.get(query)
-        response.raise_for_status()
-    except requests.exceptions.HTTPError as errh:
-        logging.error("HTTP Error:",errh)
-    except requests.exceptions.ConnectionError as errc:
-        logging.error("Error Connecting:",errc)
-    except requests.exceptions.Timeout as errt:
-        logging.error("Timeout Error:",errt)
-    except requests.exceptions.RequestException as err:
-        logging.error("Unknown error:",err)
 
-    price = response.json()["results"]["p"]
-    return float(price)
+async def fetch_all_prices():
+    """Fetching the prices for all (company, day)"""
+    async with httpx.AsyncClient() as client:
+        return await asyncio.gather(
+            *map(fetch_price, product(tickers.keys(), previous_bdays()), repeat(client),)
+        )
 
-def history(company: str) -> list[float | None]:
-    """
-    Fetch history of stock prices for passed `company` over the last `bdays_window` business days
-    Time window is a global variable for now, as the user cannot set in the frontend.
-    This part makes the application slow (20s load-time) because there is 1 API call/(day*company)
-    Next step would be to have history available in a remote bucket or database
-    OR to make concurrent requests. 
-    """
-    try:
-        ticker = tickers[company]
-    except KeyError:
-        logging.error("Unregistered company. Unable to fetch history.")
-        return []
-    
-    prices = []
-    for day in previous_bdays():
-        query = f"https://api.polygon.io/v1/open-close/{ticker}/{day}?adjusted=true&apiKey={api_key}"
-        try:
-            response = requests.get(query)
-            response.raise_for_status()
-        except requests.exceptions.HTTPError as errh:
-            logging.error("HTTP Error:",errh)
-        except requests.exceptions.ConnectionError as errc:
-            logging.error("Error Connecting:",errc)
-        except requests.exceptions.Timeout as errt:
-            logging.error("Timeout Error:",errt)
-        except requests.exceptions.RequestException as err:
-            logging.error("Unknown error:",err)
-    
-        closing_price = float(response.json()["close"])
-        prices.append(closing_price)
-
-    return prices
-
-def full_data(dataframe: bool = False) -> dict[str: list[float]] | pd.DataFrame:
-    """
-    Assemble history + current price in 1 dataset
-    Return a dictionary or a pandas dataframe
-    """
-    dates = previous_bdays() + [date.today().strftime("%Y-%m-%d")]
+def build_dataset(dataframe: bool = False) -> dict[str: list[float]] | pd.DataFrame:
+    """Organize and order data fetched through asynchronous calls"""    
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    company_day_price = loop.run_until_complete(fetch_all_prices())
+    data_dict = {}
+    for datapoint in company_day_price:
+        data_dict[(datapoint[0], datapoint[1])] = datapoint[2]
+    days = previous_bdays()
     prices = {}
     for company in tickers:
-        prices[company] = history(company) + [current_price(company)]
+            prices[company] = [data_dict[company, day] for day in days]
     if dataframe:
-        return pd.DataFrame(index = dates, data = prices)
-    return {"dates": dates} | prices
+        return pd.DataFrame(index = days, data = prices)
+    return {"dates": days} | prices
 
 def body_chart(raw_data: dict[str: list[float]]) -> dict:
     """Format data to be consumed by the frontend chart component"""
@@ -107,7 +77,7 @@ def body_chart(raw_data: dict[str: list[float]]) -> dict:
 
 def compute_metrics() -> pd.DataFrame:
     """Compute requested metrics over full data (current + history)"""
-    prices = full_data(dataframe = True)
+    prices = build_dataset(dataframe = True)
     returns = (prices.iloc[-1] - prices.iloc[0]) / prices.iloc[0]
     returns_daily = (prices - prices.shift(1)) / prices.shift(1)
     df_metrics = pd.DataFrame({
@@ -130,6 +100,7 @@ def format_row(company: str, df_metrics: pd.DataFrame) -> list[str]:
     return [company] + [str(round(100 * x, 2)) + " %" for x in df_metrics.loc[company]]
 
 def body_metrics(df_metrics: pd.DataFrame) -> dict[str: list[str]]:
+    """Format data to be consumed by the frontend Table component"""
     rows = [stringify_row(["Company"] + list(df_metrics.columns))]
     for company in tickers:
         rows.append(stringify_row(format_row(company, df_metrics)))
@@ -154,7 +125,7 @@ def home() -> dict:
 # Chart component's endpoint
 @app.get('/data')
 def data() -> dict:
-    return body_chart(full_data())
+    return body_chart(build_dataset())
 
 # Table of metrics component's endpoint
 @app.get('/metrics')
