70f06a2d6297d8388814df218a3fbd16e2d1333e
Vincent Le Goualher <v.legoualher@gmail.com>
2023-07-02 15:41:08 +0200

    Refactor project

--- workdir/backend_fastapi.py
+++ workdir/backend_fastapi.py
@@ -1,112 +1,43 @@
+from __future__ import annotations
 from fastapi import FastAPI
 import logging
 import os
-import pandas as pd
-from datetime import date, timedelta
-from math import sqrt
-import asyncio
-from itertools import repeat, product
-import httpx
+from dataclasses import dataclass, field
+import utils
 
 api_key = os.environ.get("API_KEY") # Fetched from GCP Secrets, by GCP Cloud Run
-tickers = {"Google": "GOOG", "Amazon": "AMZN", "Microsoft": "MSFT"}
-window = 10
-# Charts params
-colors = {"Google": "#8e5ea2", "Amazon": "#3cba9f", "Microsoft": "#e8c3b9"}
-fill = False
 
-def previous_bdays() -> list[str]:
-    """Return the last `window` business days, up to yesterday (at most)"""
-    bdays_window = window - 1
-    today = date.today().strftime("%Y-%m-%d")
-    yesterday = date.today() - timedelta(days = 1)
-    previous_bdays = pd.bdate_range(end = yesterday, periods = bdays_window).strftime("%Y-%m-%d")
-    return previous_bdays.tolist() + [today]
+@dataclass
+class Stocks:
+    window: int
+    tickers: list[str]
+    days: list[str] = field(init = False)
+    prices: list[tuple[str, str, float]] = field(init = False)
 
-async def fetch_price(company_day: tuple[str, str], client: httpx.AsyncClient) -> None:
-    """Fetch the closing price on a previous business day, or today's current price"""
-    company, day = company_day
-    today = date.today().strftime("%Y-%m-%d")
-    # Error handling needed
-    if day == today or day == "2023-05-29":
-        response = await client.get(f"https://api.polygon.io/v2/last/trade/{tickers[company]}?apiKey={api_key}")
-        price = response.json()["results"]["p"]
-    else:
-        response = await client.get(f"https://api.polygon.io/v1/open-close/{tickers[company]}/{day}?adjusted=true&apiKey={api_key}")
-        price = float(response.json()["close"])
-    return company, day, price
+    colors: list[str]
+    fill: bool
+    chart_payload: dict = field(init = False)
 
+    def __post_init__(self: Stocks) -> None:
+        self.days = utils.get_business_days(self.window)
+        self.prices = utils.get_prices(self.tickers, self.days, api_key)
+        self.chart_payload = self.get_chart_payload()
 
-async def fetch_all_prices():
-    """Fetching the prices for all (company, day)"""
-    async with httpx.AsyncClient() as client:
-        return await asyncio.gather(
-            *map(fetch_price, product(tickers.keys(), previous_bdays()), repeat(client),)
-        )
+    def get_chart_payload(self: Stocks) -> dict:
+        body = {}
+        body["labels"] = self.days
+        datasets = []
+        for i, ticker in enumerate(self.tickers):
+            dataset = {}
+            prices = [datapoint[2] for datapoint in self.prices if datapoint[0] == ticker]
+            dataset["data"] = [round(100 * price / prices[0], 3) for price in prices]
+            dataset["label"] = ticker
+            dataset["borderColor"] = self.colors[i]
+            dataset["fill"] = self.fill
+            datasets.append(dataset)
+        body["datasets"] = datasets
+        return body
 
-def build_dataset(dataframe: bool = False) -> dict[str: list[float]] | pd.DataFrame:
-    """Organize and order data fetched through asynchronous calls"""    
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    company_day_price = loop.run_until_complete(fetch_all_prices())
-    data_dict = {}
-    for datapoint in company_day_price:
-        data_dict[(datapoint[0], datapoint[1])] = datapoint[2]
-    days = previous_bdays()
-    prices = {}
-    for company in tickers:
-            prices[company] = [data_dict[company, day] for day in days]
-    if dataframe:
-        return pd.DataFrame(index = days, data = prices)
-    return {"dates": days} | prices
-
-def body_chart(raw_data: dict[str: list[float]]) -> dict:
-    """Format data to be consumed by the frontend chart component"""
-    body = {}
-    body["labels"] = raw_data["dates"]
-    datasets = []
-    for company in tickers:
-        dataset = {}
-        dataset["data"] = [100 * price / raw_data[company][0] for price in raw_data[company]]
-        dataset["label"] = company
-        dataset["borderColor"] = colors[company]
-        dataset["fill"] = fill
-        datasets.append(dataset)
-    body["datasets"] = datasets
-    return body
-
-def compute_metrics() -> pd.DataFrame:
-    """Compute requested metrics over full data (current + history)"""
-    prices = build_dataset(dataframe = True)
-    returns = (prices.iloc[-1] - prices.iloc[0]) / prices.iloc[0]
-    returns_daily = (prices - prices.shift(1)) / prices.shift(1)
-    df_metrics = pd.DataFrame({
-        "Return": returns,
-        "Annualized Return": returns.apply(func = lambda r: (1 + r) ** (365 / window) - 1),
-        "Annualized Volatility": returns_daily.std() * sqrt(261)
-    })
-    return df_metrics
-
-def stringify_row(row: list[str]) -> str:
-    """
-    Convert a row of data in a string.
-    The original plan was to pass fixed-width strings to the frontend
-    But Vue automatically trims whitespaces
-    """
-    return '| '.join(map(lambda s: '{:<20}'.format(s), row))
-
-def format_row(company: str, df_metrics: pd.DataFrame) -> list[str]:
-    """Format percentages for more readability"""
-    return [company] + [str(round(100 * x, 2)) + " %" for x in df_metrics.loc[company]]
-
-def body_metrics(df_metrics: pd.DataFrame) -> dict[str: list[str]]:
-    """Format data to be consumed by the frontend Table component"""
-    rows = [stringify_row(["Company"] + list(df_metrics.columns))]
-    for company in tickers:
-        rows.append(stringify_row(format_row(company, df_metrics)))
-    return {"metrics": rows}
-
-# CORS: needs refinement, necessary for the frontend to connect
 from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 origins = ["*"]
@@ -125,9 +56,10 @@ def home() -> dict:
 # Chart component's endpoint
 @app.get('/data')
 def data() -> dict:
-    return body_chart(build_dataset())
-
-# Table of metrics component's endpoint
-@app.get('/metrics')
-def metrics() -> dict:
-    return body_metrics(compute_metrics())
+    stocks = Stocks(
+        tickers = ["GOOG", "AMZN", "MSFT"],
+        window = 10,
+        colors = ["#8e5ea2", "#3cba9f", "#e8c3b9"],
+        fill = False
+    )
+    return stocks.chart_payload
